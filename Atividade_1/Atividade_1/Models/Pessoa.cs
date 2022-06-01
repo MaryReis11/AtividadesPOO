cb170335fe171f88a6a26bc289342fa227034e2f
Hellen Mariane <maryreisl42@gmail.com>
2022-06-01 20:51:08 -0300

    Question1

---
+++ Atividade_1/Atividade_1/Models/Pessoa.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_1.Models
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private DateTime dataDeNascimento;
+        private float altura;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+
+
+        public DateTime DataDeNascimento
+        {
+            get
+            {
+                return dataDeNascimento;
+            }
+            set
+            {
+                dataDeNascimento = value;
+            }
+        }
+
+
+        public float Altura
+        {
+            get
+            {
+                return altura;
+            }
+            set
+            {
+                altura = value;
+            }
+
+        }
+
+        //Metodo para imprimir dados da pessoa
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("A data de nascimento da pessoa é: " + dataDeNascimento);
+            Console.WriteLine("A altura da pessoa é : " + altura);
+        }
+
+        //Metodo para calcular idade da pessoa
+        public int CalculoIdade()
+        {
+            return DateTime.Today.Year - dataDeNascimento.Year;
+        }
+    }
+}
+
---
+++ Atividade_1/Atividade_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Atividade_1.Models;
+
+namespace Atividade_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando a classe pessoa
+            Pessoa p = new Pessoa();
+
+            //atribuindo os valores
+            p.Nome = "Hellen";
+            p.Altura = 1.71f;
+
+            //instancia do metado dateTime
+            p.DataDeNascimento = new DateTime(2002, 11, 9);//ano, mês e dia
+
+            //Invocando metodo imprimirDados para os dados serem preenchidos na tela
+            p.ImprimirDados();
+
+            int idade = p.CalculoIdade();
+
+            Console.WriteLine("Idade: " + idade);
+
+
+            Console.ReadKey();
+        }
+    }
+}
